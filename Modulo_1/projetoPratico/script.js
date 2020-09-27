bf73832c87969f7b451b14f7d9bc0efab88a4f6b
ssraf <rafaelsanchezsouza@gmail.com>
2020-09-27 19:13:33 -0300

    range input and text input in sync

--- Modulo_1/projetoPratico/script.js
+++ Modulo_1/projetoPratico/script.js
@@ -1,22 +1,25 @@
 function updateRed(redSlider) {
   changeColor();
-  document.getElementById('Red').value = redSlider;
+  document.getElementById('rangeRed').value = redSlider;
+  document.getElementById('textRed').value = redSlider;
 }
 function updateGreen(greenSlider) {
   changeColor();
-  console.log('Update Green');
-  document.getElementById('Green').value = greenSlider;
+  //  console.log('Update Green');
+  document.getElementById('rangeGreen').value = greenSlider;
+  document.getElementById('textGreen').value = greenSlider;
 }
 function updateBlue(blueSlider) {
   changeColor();
-  document.getElementById('Blue').value = blueSlider;
+  document.getElementById('rangeBlue').value = blueSlider;
+  document.getElementById('textBlue').value = blueSlider;
 }
 
 function changeColor() {
-  console.log('Change Color');
-  var red = document.getElementById('Red').value;
-  var green = document.getElementById('Green').value;
-  var blue = document.getElementById('Blue').value;
+  //console.log('Change Color');
+  var red = document.getElementById('rangeRed').value;
+  var green = document.getElementById('rangeGreen').value;
+  var blue = document.getElementById('rangeBlue').value;
   // document.getElementByClassName('square').style.background =
   //   'rgb(' + red + ',' + green + ',' + blue + ')';
   var all = document.getElementsByClassName('square');
