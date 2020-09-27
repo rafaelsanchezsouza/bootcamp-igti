7f56f43de53ce77b18ed2fbf226b12fa688c8238
ssraf <rafaelsanchezsouza@gmail.com>
2020-09-27 18:51:18 -0300

    squarte background color changing

--- Modulo_1/projetoPratico/script.js
+++ Modulo_1/projetoPratico/script.js
@@ -1,9 +1,26 @@
 function updateRed(redSlider) {
-  document.getElementById('textRed').value = redSlider;
+  changeColor();
+  document.getElementById('Red').value = redSlider;
 }
 function updateGreen(greenSlider) {
-  document.getElementById('textGreen').value = greenSlider;
+  changeColor();
+  console.log('Update Green');
+  document.getElementById('Green').value = greenSlider;
 }
 function updateBlue(blueSlider) {
-  document.getElementById('textBlue').value = blueSlider;
+  changeColor();
+  document.getElementById('Blue').value = blueSlider;
+}
+
+function changeColor() {
+  console.log('Change Color');
+  var red = document.getElementById('Red').value;
+  var green = document.getElementById('Green').value;
+  var blue = document.getElementById('Blue').value;
+  // document.getElementByClassName('square').style.background =
+  //   'rgb(' + red + ',' + green + ',' + blue + ')';
+  var all = document.getElementsByClassName('square');
+  for (var i = 0; i < all.length; i++) {
+    all[i].style.background = 'rgb(' + red + ',' + green + ',' + blue + ')';
+  }
 }
