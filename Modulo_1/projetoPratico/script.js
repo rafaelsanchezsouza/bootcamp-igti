666a16d3cdcfd6ca227f374e78a339a96bc641b4
ssraf <rafaelsanchezsouza@gmail.com>
2020-09-27 18:12:41 -0300

    working on practical project

---
+++ Modulo_1/projetoPratico/script.js
@@ -0,0 +1,9 @@
+function updateRed(redSlider) {
+  document.getElementById('textRed').value = redSlider;
+}
+function updateGreen(greenSlider) {
+  document.getElementById('textGreen').value = greenSlider;
+}
+function updateBlue(blueSlider) {
+  document.getElementById('textBlue').value = blueSlider;
+}
---
+++ Modulo_1/script.js
@@ -0,0 +1,5 @@
+console.log('Olá, Mundo');
+
+var title = document.querySelector('h1');
+
+// title.textContent = 'Modificado';
