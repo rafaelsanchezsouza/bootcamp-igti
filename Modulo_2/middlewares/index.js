6c8656b8820e4aa8962f82ddacf93f06c48cac5f
ssraf <rafaelsanchezsouza@gmail.com>
2020-10-13 17:37:28 -0300

    middlewares init

---
+++ Modulo_2/middlewares/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(new Date());
+  next();
+});
+
+app.get('./teste', (req, res) => {
+  res.end();
+});
+
+app.listen(3000, () => {
+  console.log('API Started');
+});
